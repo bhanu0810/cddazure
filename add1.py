51bb417310d308ffae8c47c24e3935c3f870364d
S V MANIKANTA SAI BHANU <116937257+bhanu0810@users.noreply.github.com>
2025-01-24 10:17:37 +0530

    Add files via upload

---
+++ add1.py
@@ -0,0 +1,13 @@
+# Function to add two numbers
+def add_numbers(a, b):
+    return a + b
+
+# Example usage
+num1 = 5
+num2 = 3
+
+# Call the function and store the result
+result = add_numbers(num1, num2)
+
+# Print the result
+print(f"The sum of {num1} and {num2} is {result}")
